ff7280c2154ec92416380f934b31ed589e537171
Abdus Sattar <abdussattarsoftit@gmail.com>
2024-10-15 13:13:12 +0600

    Add a converter function to construct 1D array to 2D

---
+++ src/ts/construct2Darray.ts
@@ -0,0 +1,35 @@
+/* You are given a 0-indexed 1-dimensional (1D) integer array original,
+* and two integers, m and n. You are tasked with creating a 2-dimensional (2D)
+ array with  m rows and n columns using all the elements from original.
+
+* The elements from indices 0 to n - 1 (inclusive) of original should form
+the first row of the constructed 2D array, the elements
+from indices n to 2 * n - 1 (inclusive) should form the second row of the
+constructed 2D array, and so on.
+
+* Return an m x n 2D array constructed according to the above procedure,
+* or an empty 2D array if it is impossible.
+*/
+
+interface Construct2D {
+  (original: number[], m: number, n: number): number[][];
+}
+
+const construct2Darrary: Construct2D = (original, m, n) => {
+  const converted: number[][] = [];
+  // check the 2D array construction is possible or not
+  if (original.length !== m * n) return converted;
+
+  // loop through the original array m times to construct a 2D array
+  for (let i = 0; i < m; i++) {
+    const row: number[] = original.slice(i * n, (i + 1) * n);
+    converted.push(row);
+  }
+
+  return converted;
+};
+
+console.log(construct2Darrary([1, 2, 3, 4], 2, 2)); // [[1,2],[3,4]]
+console.log(construct2Darrary([1, 2, 3, 4], 2, 3)); // []
+console.log(construct2Darrary([1, 2, 3, 4, 5, 6, 7, 8], 4, 2)); // [[1,2],[3,4],[5,6],[7,8]]
+console.log(construct2Darrary([1, 2, 3, 4, 5, 6, 7, 8], 2, 4)); // [[1,2,3,4],[5,6,7,8]]
