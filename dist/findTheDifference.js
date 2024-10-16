c0904f78e2beffb0cd29aefa61bc4ce398b34eeb
Abdus Sattar <abdussattarsoftit@gmail.com>
2024-10-16 16:05:51 +0600

    Update insertion sort method

---
+++ dist/findTheDifference.js
@@ -0,0 +1,18 @@
+"use strict";
+const findTheDifference = (s, t) => {
+    let sumS = 0, sumT = 0;
+    // Sum the ASCII values of s
+    for (let char of s) {
+        sumS += char.charCodeAt(0);
+    }
+    // Sum the ASCII values of t
+    for (let char of t) {
+        sumT += char.charCodeAt(0);
+    }
+    // The difference is the ASCII value of the extra character
+    return String.fromCharCode(sumT - sumS);
+};
+// Test cases
+console.log(findTheDifference("abcd", "abcde")); // Output: 'e'
+console.log(findTheDifference("", "y")); // Output: 'y'
+console.log(findTheDifference("a", "aa")); // Output: 'a'
